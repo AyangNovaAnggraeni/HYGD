b2144ecc37da9fc9b3d605e591db61cedacdf690
AyangNovaAnggraeni <ayangnovaanggraeni371@gmail.com>
2026-03-28 18:37:50 +0700

    editing app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 from PIL import Image
 import cv2
 from torchvision import models
-from pytorch_grad_cam import GradCAM
+# from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.image import show_cam_on_image
 
 import sys
